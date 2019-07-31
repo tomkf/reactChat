c8281a6dde21365a86fb5fd1ac4cfc56a35604bc
Thomas Ferris <thomaskeithferris@gmail.com>
2019-07-31 00:55:51 -0400

    element to iterate message

---
+++ src/MessageList.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Message from './Message.jsx';
+
+export default class MessageList extends React.Component {
+
+
+  render() {
+    const displaymessages = this.props.messages.map(message => (
+      message.type === "incomingNotification" ?
+         (<div className="notification" key={message.id}>
+            <span className="notification-content">{message.content}</span>
+          </div>  
+        )
+        : 
+        <Message id={message.id} key={message.id} message={message} />
+        ))
+    return (
+    <main className="messages">
+        <h4>Comments</h4>
+      {displaymessages}
+    </main>
+    )
+  }
+};
+
+
